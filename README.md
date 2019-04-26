98daa083345db91f4a55b73cc55b36cf8d353538
Thanh Nhan Pham <ptnhan@tma.com.vn>
2019-04-26 12:13:07 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Description
+
+The PHP CMS Starter Kit is a common repository for PHP scripts related to Gigya's various PHP-based CMS modules.
+
+# Release notes
+## 1.2.2
+- [X] Bug fix related to field mapping
+
+## 1.2.1
+- [X] Bug fixes related to field mapping
+
+## 1.2
+- [X] Breaking change: changed encryption library from the deprecated MCrypt to OpenSSL
+- [X] Bug fixes
+
+## 1.1.9.4
+- [X] Minor bug fixes
---
+++ src/sdk/GigyaApiRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Gigya\CmsStarterKit\sdk;
+/**
+ * Created by PhpStorm.
+ * User: Yaniv Aran-Shamir
+ * Date: 1/4/16
+ * Time: 4:59 PM
+ */
+
+class GigyaApiRequest extends GSRequest
+
+{
+	/**
+	 * @param null $timeout
+	 *
+	 * @return GSResponse
+	 *
+	 * @throws \Exception
+	 * @throws GSApiException
+	 */
+	public function send( $timeout = null ) {
+		$res = parent::send( $timeout );
+		if ($res->getErrorCode() == 0) {
+			return $res;
+		}
+
+		throw new GSApiException($res->getErrorMessage(), $res->getErrorCode(), $res->getResponseText(), $res->getString("callId", "N/A"));
+	}
+
+	/**
+	 * GSRequestNg constructor.
+	 *
+	 * @param string   $apiKey
+	 * @param string   $secret
+	 * @param string   $apiMethod
+	 * @param GSObject $params
+	 * @param string   $dataCenter
+	 * @param bool     $useHTTPS
+	 * @param null     $userKey
+	 *
+	 * @throws \Exception
+	 */
+	public function __construct( $apiKey, $secret, $apiMethod, $params, $dataCenter, $useHTTPS = true, $userKey = null ) {
+		parent::__construct( $apiKey , $secret, $apiMethod, $params, $useHTTPS, $userKey );
+        $this->setAPIDomain($dataCenter);
+		$this->setCAFile(realpath(dirname(__FILE__) . "/cacert.pem"));
+	}
+}
